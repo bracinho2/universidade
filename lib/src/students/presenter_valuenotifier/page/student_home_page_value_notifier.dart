3f367a17c499e96b574f19e811c109c524f5c8a6
bracinho2 <bracinho2@hotmail.com>
2022-04-14 14:48:12 -0300

    Atualização do Valuenotifybuilder

--- lib/src/students/presenter_valuenotifier/page/student_home_page_value_notifier.dart
+++ lib/src/students/presenter_valuenotifier/page/student_home_page_value_notifier.dart
@@ -19,9 +19,6 @@ class _StudentHomePageValueNotifierState
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
-      context.read<StudentHomeStoreValueNotifier>().fetchStudents();
-    });
   }
 
   @override
@@ -32,82 +29,83 @@ class _StudentHomePageValueNotifierState
   @override
   Widget build(BuildContext context) {
     final store = context.watch<StudentHomeStoreValueNotifier>();
-    Widget child = Container();
-
-    final state = store.value;
-
-    if (state is EmptyStudentState) {
-      child = const Center(
-        child: Text('Não existem Estudantes Cadastrados!'),
-      );
-    }
-    if (state is LoadingStudentState) {
-      child = const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-    if (state is ErrorStudentState) {
-      child = Center(
-        child: Text(state.message),
-      );
-    }
-    if (state is SuccessStudentState) {
-      child = ListView.builder(
-        itemCount: state.students.length,
-        itemBuilder: (context, index) {
-          final student = state.students[index];
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 5,
-            ),
-            child: ListTile(
-              title: Text(
-                student.name,
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              subtitle: Text(
-                student.sobrenome,
-              ),
-              trailing: Text(
-                '# ' + student.id,
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.green,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(8),
-                ),
-              ),
-              tileColor: Colors.white12,
-              selectedTileColor: Colors.black,
-              selectedColor: Colors.amber,
-              onTap: () {
-                print('ID do Estudante: ' + student.id);
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (_) => PerformancePage(student: student),
-                    ));
-              },
-            ),
-          );
-        },
-      );
-    }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Students - ValueNotifier'),
       ),
-      body: child,
+      body: ValueListenableBuilder(
+        valueListenable: store,
+        builder: (context, state, child) {
+          if (state is EmptyStudentState) {
+            child = const Center(
+              child: Text('Não existem Estudantes Cadastrados!'),
+            );
+          }
+          if (state is LoadingStudentState) {
+            child = const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is ErrorStudentState) {
+            child = Center(
+              child: Text(state.message),
+            );
+          }
+          if (state is SuccessStudentState) {
+            child = ListView.builder(
+              itemCount: state.students.length,
+              itemBuilder: (context, index) {
+                final student = state.students[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 5,
+                  ),
+                  child: ListTile(
+                    title: Text(
+                      student.name,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    subtitle: Text(
+                      student.sobrenome,
+                    ),
+                    trailing: Text(
+                      '# ' + student.id,
+                      style: const TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.green,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                    tileColor: Colors.white12,
+                    selectedTileColor: Colors.black,
+                    selectedColor: Colors.amber,
+                    onTap: () {
+                      print('ID do Estudante: ' + student.id);
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => PerformancePage(student: student),
+                          ));
+                    },
+                  ),
+                );
+              },
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
